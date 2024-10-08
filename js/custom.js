c7140d2c706abfde8023cd3a4cca52d02adaa6db
yelanishashikala <sayelani@gmail.com>
2024-10-08 19:34:36 +0530

    Art Gallery

---
+++ js/custom.js
@@ -0,0 +1,102 @@
+(function() {
+	'use strict';
+
+	var tinyslider = function() {
+		var el = document.querySelectorAll('.testimonial-slider');
+
+		if (el.length > 0) {
+			var slider = tns({
+				container: '.testimonial-slider',
+				items: 1,
+				axis: "horizontal",
+				controlsContainer: "#testimonial-nav",
+				swipeAngle: false,
+				speed: 700,
+				nav: true,
+				controls: true,
+				autoplay: true,
+				autoplayHoverPause: true,
+				autoplayTimeout: 3500,
+				autoplayButtonOutput: false
+			});
+		}
+	};
+	tinyslider();
+
+	
+
+
+	var sitePlusMinus = function() {
+
+		var value,
+    		quantity = document.getElementsByClassName('quantity-container');
+
+		function createBindings(quantityContainer) {
+	      var quantityAmount = quantityContainer.getElementsByClassName('quantity-amount')[0];
+	      var increase = quantityContainer.getElementsByClassName('increase')[0];
+	      var decrease = quantityContainer.getElementsByClassName('decrease')[0];
+	      increase.addEventListener('click', function (e) { increaseValue(e, quantityAmount); });
+	      decrease.addEventListener('click', function (e) { decreaseValue(e, quantityAmount); });
+	    }
+
+	    function init() {
+	        for (var i = 0; i < quantity.length; i++ ) {
+						createBindings(quantity[i]);
+	        }
+	    };
+
+	    function increaseValue(event, quantityAmount) {
+	        value = parseInt(quantityAmount.value, 10);
+
+	        console.log(quantityAmount, quantityAmount.value);
+
+	        value = isNaN(value) ? 0 : value;
+	        value++;
+	        quantityAmount.value = value;
+	    }
+
+	    function decreaseValue(event, quantityAmount) {
+	        value = parseInt(quantityAmount.value, 10);
+
+	        value = isNaN(value) ? 0 : value;
+	        if (value > 0) value--;
+
+	        quantityAmount.value = value;
+	    }
+	    
+	    init();
+		
+	};
+	sitePlusMinus();
+
+
+})()
+
+$(document).ready(function($) {
+    $('.card-slider').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 2000,
+      arrows: true,
+      responsive: [{
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+         breakpoint: 400,
+         settings: {
+            arrows: false,
+            slidesToShow: 1,
+            slidesToScroll: 1
+         }
+      }]
+  });
+});
+
---
+++ js/slider.js
@@ -0,0 +1,35 @@
+var swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 3,
+      slideShadows: true
+    },
+    loop: true,
+    autoplay: {
+      delay: 3000, // 3 seconds delay between slides
+      disableOnInteraction: false, // keeps autoplay active after user interaction
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2
+      },
+      768: {
+        slidesPerView: 1
+      },
+      1024: {
+        slidesPerView: 2
+      },
+      1560: {
+        slidesPerView: 3
+      }
+    }
+});
